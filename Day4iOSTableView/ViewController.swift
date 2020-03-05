af95db8936c6d51055fd234a2488e44f7513b75e
Nikita Sandhu <sandhunikita0096@gmail.com>
2020-03-04 20:53:23 -0500

    table view

--- Day4iOSTableView/ViewController.swift
+++ Day4iOSTableView/ViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tblCountry: UITableView!
+    
+    var countryNames = ["Afganistan", "Angola", "Aruba", "Canada", "India", "Bahamas", "Ghana", "France", "Nepal", "Kenya", "Bermudas", "Danemark"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
 
 }
 
+extension ViewController : UITableViewDataSource, UITableViewDelegate{
+    
+    func numberOfSections(in tableView: UITableView) -> Int{
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return countryNames.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CountryCell")
+        
+        cell?.textLabel?.text = countryNames[indexPath.row]
+        
+        return cell!
+    }
+    
+}
